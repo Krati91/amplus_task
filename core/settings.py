ed84f8493180053f3a7dc48d0c533674b18a6980
krati.chaturvedi@gmail.com <krati.chaturvedi@gmail.com>
2021-10-09 18:26:47 +0530

    config file

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +79,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'solar_plant',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PASSWORD': 'admin',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'HOST': config('DATABASE_HOST'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
         'PORT': '5432'
     }
 }
